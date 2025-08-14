5488718e022f33424d37da943485ffb7d4d0833b
PERFEX DEV 360 <perfexdev360@gmail.com>
2025-08-14 17:08:54 +0500

    Add licensing activation and update APIs with download checks

--- app/Models/FileArtifact.php
+++ app/Models/FileArtifact.php
@@ -3,8 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class FileArtifact extends Model
 {
-    //
+    protected $fillable = [
+        'version_id',
+        'path',
+        'size',
+        'hash',
+        'signature',
+    ];
+
+    public function version(): BelongsTo
+    {
+        return $this->belongsTo(Version::class);
+    }
 }
