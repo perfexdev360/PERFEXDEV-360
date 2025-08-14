9c60e77cfff8970eaee4f305107fcd52a78d3f41
PERFEX DEV 360 <perfexdev360@gmail.com>
2025-08-14 16:56:00 +0500

    Add Stripe and PayPal webhook handlers

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Webhook\StripeWebhookController;
+use App\Http\Controllers\Webhook\PayPalWebhookController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/webhooks/stripe', [StripeWebhookController::class, 'handle']);
+Route::post('/webhooks/paypal', [PayPalWebhookController::class, 'handle']);
