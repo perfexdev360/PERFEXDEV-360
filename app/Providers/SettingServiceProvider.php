8459308b8ecb498e02619f943c903ce62c8b3ce7
PERFEX DEV 360 <perfexdev360@gmail.com>
2025-08-15 20:56:25 +0500

    Add Docker setup and configurable settings

---
+++ app/Providers/SettingServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\Setting;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\ServiceProvider;
+
+class SettingServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        //
+    }
+
+    public function boot(): void
+    {
+        if (Schema::hasTable('settings')) {
+            $settings = Setting::pluck('value', 'key')->toArray();
+            $config = [];
+            foreach ($settings as $key => $value) {
+                Arr::set($config, $key, $value);
+            }
+            config(['settings' => $config]);
+        }
+    }
+}
