43f73d0e23150e67779280969e9394c4991fedcc
MubashirRiaz7 <mubashirriaz75@gmail.com>
2025-08-15 15:23:04 +0500

    POST

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -1,8 +1,35 @@
-@props(['label' => 'Menu'])
+@props(['align' => 'right', 'width' => '48', 'contentClasses' => 'py-1 bg-white dark:bg-gray-700'])
 
-<div x-data="{ open: false }" class="relative" @click.outside="open=false">
-    <button @click="open=!open" class="px-4 py-2 bg-gray-200 dark:bg-gray-700 rounded">{{ $label }}</button>
-    <div x-show="open" x-transition class="absolute right-0 mt-2 w-48 bg-white dark:bg-gray-800 rounded shadow-lg z-50">
-        {{ $slot }}
+@php
+$alignmentClasses = match ($align) {
+    'left' => 'ltr:origin-top-left rtl:origin-top-right start-0',
+    'top' => 'origin-top',
+    default => 'ltr:origin-top-right rtl:origin-top-left end-0',
+};
+
+$width = match ($width) {
+    '48' => 'w-48',
+    default => $width,
+};
+@endphp
+
+<div class="relative" x-data="{ open: false }" @click.outside="open = false" @close.stop="open = false">
+    <div @click="open = ! open">
+        {{ $trigger }}
+    </div>
+
+    <div x-show="open"
+            x-transition:enter="transition ease-out duration-200"
+            x-transition:enter-start="opacity-0 scale-95"
+            x-transition:enter-end="opacity-100 scale-100"
+            x-transition:leave="transition ease-in duration-75"
+            x-transition:leave-start="opacity-100 scale-100"
+            x-transition:leave-end="opacity-0 scale-95"
+            class="absolute z-50 mt-2 {{ $width }} rounded-md shadow-lg {{ $alignmentClasses }}"
+            style="display: none;"
+            @click="open = false">
+        <div class="rounded-md ring-1 ring-black ring-opacity-5 {{ $contentClasses }}">
+            {{ $content }}
+        </div>
     </div>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Admin\LeadController;
 
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::patch('/admin/leads/{lead}/stage', [LeadController::class, 'moveStage']);
-use App\Http\Controllers\Webhook\StripeWebhookController;
-use App\Http\Controllers\Webhook\PayPalWebhookController;
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-
-
-
-use App\Http\Controllers\Frontend\BlogController;
-use App\Http\Controllers\Frontend\ContactController;
-use App\Http\Controllers\Frontend\HomeController;
-use App\Http\Controllers\Frontend\ProductController;
-use App\Http\Controllers\Frontend\ServiceController;
-
-Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::get('/products', [ProductController::class, 'index'])->name('products.index');
-Route::get('/services', [ServiceController::class, 'index'])->name('services.index');
-Route::get('/blog', [BlogController::class, 'index'])->name('blog.index');
-Route::get('/contact', [ContactController::class, 'index'])->name('contact.index');
-Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
-Route::post('/webhooks/stripe', [StripeWebhookController::class, 'handle']);
-Route::post('/webhooks/paypal', [PayPalWebhookController::class, 'handle']);
+require __DIR__.'/auth.php';
