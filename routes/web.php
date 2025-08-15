b48fba07eff872c94bfc57dad739ec25b0b759ef
MubashirRiaz7 <mubashirriaz75@gmail.com>
2025-08-15 15:25:49 +0500

    HomeController

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,12 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+Route::get('/services', [ServiceController::class, 'index'])->name('services.index');
+Route::get('/blog', [BlogController::class, 'index'])->name('blog.index');
+Route::get('/contact', [ContactController::class, 'index'])->name('contact.index');
+Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
