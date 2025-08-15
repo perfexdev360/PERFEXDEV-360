743a04d4cfe4b67d4c83f34c3f0cfb90413cc705
PERFEX DEV 360 <perfexdev360@gmail.com>
2025-08-15 21:03:59 +0500

    Add sitemap and SEO middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,7 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->append(App\Http\Middleware\CanonicalRedirect::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,12 @@ use App\Http\Controllers\Frontend\ProductController;
 use App\Http\Controllers\Frontend\ServiceController;
 use App\Http\Controllers\Frontend\BlogController;
 use App\Http\Controllers\Frontend\ContactController;
+use Spatie\Sitemap\Sitemap;
+use Spatie\Sitemap\Tags\Url;
+use Spatie\Sitemap\Tags\Sitemap as SitemapTag;
+use App\Models\BlogPost;
+use App\Models\Product;
+use App\Models\CaseStudy;
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/products', [ProductController::class, 'index'])->name('products.index');
 Route::get('/products/{product:slug}', [ProductController::class, 'show'])->name('products.show');
@@ -14,6 +20,36 @@ Route::get('/services', [ServiceController::class, 'index'])->name('services.ind
 Route::get('/blog', [BlogController::class, 'index'])->name('blog.index');
 Route::get('/contact', [ContactController::class, 'index'])->name('contact.index');
 Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
+
+Route::get('/sitemap.xml', function () {
+    return Sitemap::create()
+        ->add(SitemapTag::create('/sitemap-posts.xml'))
+        ->add(SitemapTag::create('/sitemap-products.xml'))
+        ->add(SitemapTag::create('/sitemap-cases.xml'))
+        ->toResponse(request());
+});
+
+Route::get('/sitemap-posts.xml', function () {
+    $sitemap = Sitemap::create();
+    BlogPost::all()->each(fn ($post) => $sitemap->add(Url::create(url('/blog/'.$post->slug))));
+    return $sitemap->toResponse(request());
+});
+
+Route::get('/sitemap-products.xml', function () {
+    $sitemap = Sitemap::create();
+    Product::all()->each(fn ($product) => $sitemap->add(Url::create(url('/products/'.$product->slug))));
+    return $sitemap->toResponse(request());
+});
+
+Route::get('/sitemap-cases.xml', function () {
+    $sitemap = Sitemap::create();
+    CaseStudy::all()->each(fn ($case) => $sitemap->add(Url::create(url('/cases/'.$case->slug))));
+    return $sitemap->toResponse(request());
+});
+
+Route::get('/robots.txt', function () {
+    return response()->view('components.robots')->header('Content-Type', 'text/plain');
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
